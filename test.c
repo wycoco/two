c43d22f2dfa41c3ec30a1265b82da54fd49e1098
wycoco <84973618@qq.com>
2019-10-25 10:30:16 +0800

    提交test.c

--- test.c
+++ test.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 void main()
 {
-    int i;
-    printf("Hello World!");
+	int i = 10;
+	sq(i);
+    printf("Hello Word!!!");
+}
+void sort(i);
+{
+	printf(" the  is equal is %d", &i);
 }
